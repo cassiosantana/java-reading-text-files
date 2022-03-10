7b2c1026e8d50569607bad90c38adf5eebd7464f
Cassio Santana <cassioo.roger@gmail.com>
2022-03-09 21:24:06 -0300

    Good solution using try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,32 +9,17 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "C:\\in.txt";
-		
-		BufferedReader br = null;
-		FileReader fr = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
 			String line = br.readLine();
-			
+
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-			
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null)
-					br.close();
-				if (fr != null)
-					fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
