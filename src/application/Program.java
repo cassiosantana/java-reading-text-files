d294f8f15243af4df79fe3e58d039bbd1ddde7e4
Cassio Santana <cassioo.roger@gmail.com>
2022-03-09 21:14:31 -0300

    Verbose solution to read text files with classes FileReader and BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,39 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		String path = "C:\\in.txt";
 		
-		File file = new File("C:\\in.txt");
-		Scanner sc = null;
+		BufferedReader br = null;
+		FileReader fr = null;
 		
 		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
+			
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		} finally {
-			if (sc != null) {
-				sc.close();
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
